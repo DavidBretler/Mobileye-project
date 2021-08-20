339912eb8a0015b31929a554aff16d565d5b548d
nbarkoch <57592818+nbarkoch@users.noreply.github.com>
2021-08-20 18:02:10 +0300

    part 2

--- part2_api.py
+++ part2_api.py
@@ -1,3 +1,5 @@
+import cv2
+
 from main import test_find_tfl_lights
 
 try:
@@ -20,6 +22,11 @@ except ImportError:
     raise
 
 
+
+def create_labels(red_x, red_y, green_x, green_y):
+    pass
+
+
 def main(argv=None):
     """It's nice to have a standalone tester for the algorithm.
     Consider looping over some images from here, so you can manually exmine the results
@@ -31,10 +38,7 @@ def main(argv=None):
     parser.add_argument("-j", "--json", type=str, help="Path to json GT for comparison")
     parser.add_argument('-d', '--dir', type=str, help='Directory to scan images in')
     args = parser.parse_args(argv)
-    default_base = r"C:\Users\naorb\Desktop\Scale-up\projects\mobileye\leftImg8bit\leftImg8bit\test"
-
-    if args.dir is None:
-        args.dir = default_base
+    default_base = r"C:\Users\naorb\Desktop\Scale-up\projects\mobileye\leftImg8bit\leftImg8bit\train"
 
     # for filename in os.listdir(args.dir):
     #     flist = glob.glob(os.path.join(args.dir, filename, '*_leftImg8bit.png'))
@@ -45,13 +49,15 @@ def main(argv=None):
     #             json_fn = None
     #         test_find_tfl_lights(image, json_fn)
     counter = 1
-    flist = glob.glob(os.path.join(args.dir, "berlin", '*_leftImg8bit.png'))
+    flist = glob.glob(os.path.join(default_base, "aachen", '*_leftImg8bit.png'))
     for image in flist:
-        json_fn = image.replace('_leftImg8bit.png', '_gtFine_polygons.json')
+        json_fn = image.replace('_leftImg8bit.png', '_gtFine_labelIds.png').replace('leftImg8bit', 'gtFine', 2)
         print(counter)
         if not os.path.exists(json_fn):
             json_fn = None
         test_find_tfl_lights(image, json_fn)
+
+
         counter += 1
 
     if len(flist):
@@ -60,6 +66,10 @@ def main(argv=None):
         print("Bad configuration?? Didn't find any picture to show")
     plt.show(block=True)
 
-
-if __name__ == '__main__':
-    main()
+loaded_arr = np.fromfile("data.bin",  dtype=np.uint8)
+load_original_arr = loaded_arr.reshape(160, 80, 3)
+plt.imshow(load_original_arr)
+plt.show()
+print("fool")
+# if __name__ == '__main__':
+#     main()
