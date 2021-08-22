04d803d43600f3a4a3586f80a661a1a4d331dffc
nbarkoch <57592818+nbarkoch@users.noreply.github.com>
2021-08-22 13:22:22 +0300

    Add files via upload
    added functions for building datasets

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import heapq
 
+import cv2
 from matplotlib.testing.compare import compare_images
 from numpy import amax
 # from sympy.stats.frv_types import scipy
 import scipy
+
 try:
     import os
     import json
@@ -17,11 +19,60 @@ try:
     from PIL import Image, ImageEnhance, ImageFilter
 
     import matplotlib.pyplot as plt
-
+    import matplotlib
 
 except ImportError:
     print("Need to fix the installation")
     raise
+
+data_path = "data.bin"
+labels_path = "labels.bin"
+
+
+def stam(image):
+
+    # plt.imshow(image)
+    # plt.show()
+    # split the image into its BGR components
+    mask_red = np.all(image[:,:] <= [180, 235, 235], axis=-1)
+    mask_green = np.all(image[:,:] <= [245, 237, 245], axis=-1)
+    mask_blue = np.all(image[:,:] <= [245, 245, 190], axis=-1)
+
+    mask_white = np.ma.mask_or(np.all(image[:, :] >= [200, 200, 200], axis=-1), np.all(image[:, :] <= [195, 195, 195], axis=-1))
+    mask = np.ma.mask_or(np.ma.mask_or(mask_red, mask_green), np.ma.mask_or(mask_blue, mask_white))
+    image[mask] = [0, 0, 0]
+
+
+
+    image[np.logical_not(mask)] = [255, 255, 255]
+
+    # plt.imshow(image)
+    # plt.show()
+    blurred = cv2.GaussianBlur(image, (17, 17), 0)
+    fixed = cv2.threshold(blurred, 50, 255, cv2.THRESH_BINARY)[1]
+
+    # plt.imshow(fixed)
+    # plt.show()
+    # (B, G, R) = cv2.split(image)
+    # # find the maximum pixel intensity values for each
+    # # (x, y)-coordinate,, then set all pixel values less
+    # # than M to zero
+    #
+    # # R = cv2.threshold(R, 90, 255, cv2.THRESH_BINARY)[1]
+    # # G = cv2.threshold(G, 90, 255, cv2.THRESH_BINARY)[1]
+    # # B = cv2.threshold(B, 90, 255, cv2.THRESH_BINARY)[1]
+    #
+    # M = np.maximum(np.maximum(R, G), B)
+    #
+    #
+    #
+    # R[R < M] = 0
+    # G[G < M] = 0
+    # B[B < M] = 0
+    # # merge the channels back together and return the image
+    # final = cv2.merge([B, G, R])
+
+    return fixed
 
 
 def red_detection(img):
@@ -29,7 +80,7 @@ def red_detection(img):
     img_new = img.copy()
     for j in range(len(img_new)):
         for i in range(len(img_new[j])):
-            if img[j][i][0] > img[j][i][1]+epsilon and img[j][i][0] > img[j][i][2]+epsilon:
+            if img[j][i][0] > img[j][i][1] + epsilon and img[j][i][0] > img[j][i][2] + epsilon:
                 img_new[j][i] = [255, 255, 255]
             else:
                 img_new[j][i] = [0, 0, 0]
@@ -38,7 +89,7 @@ def red_detection(img):
 
 def check_red(pixel_colors):
     epsilon = 10
-    return pixel_colors[0] > pixel_colors[1]+epsilon and pixel_colors[0] > pixel_colors[2]+epsilon
+    return pixel_colors[0] > pixel_colors[1] + epsilon and pixel_colors[0] > pixel_colors[2] + epsilon
 
 
 # paklocalmax
@@ -52,9 +103,9 @@ def n_max(ary, n):
                 lst = sorted(lst, key=lambda x: x[0])
             else:
                 # [[1,4,3,6], [3,4,1,7], [9,1,1]]
-                    if ary[i][j] > ary[0][0]:
-                        lst[0] = (ary[i][j], i, j)
-                        lst = sorted(lst, key=lambda x: x[0])
+                if ary[i][j] > ary[0][0]:
+                    lst[0] = (ary[i][j], i, j)
+                    lst = sorted(lst, key=lambda x: x[0])
     return lst
 
 
@@ -94,25 +145,40 @@ def find_tfl_lights(c_image: np.ndarray, cv2=None, **kwargs):
 
 
 
-
     gray_image = np.dot(c_image[..., :3], [0.2125, 0.7154, 0.0721])
+
+
+    # gray_image = c_image.copy()[:,:,0]
+    # plt.imshow(gray_image)
+    # plt.show()
+
+    kernel = np.array(Image.open('k3.png').convert('L'))
+    kernel = kernel.astype('f')
+    offset = kernel.sum() / (len(kernel[0]) * len(kernel))
+
     for j in range(len(gray_image)):
         for i in range(len(gray_image[j])):
             # cou = cou + kernel[j][i]
-            gray_image[j][i] = float(gray_image[j][i]) - 31946 / (21 * 19)
+            gray_image[j][i] = float(gray_image[j][i]) - offset
 
-    kernel = np.array(Image.open('kernal.jpg').convert('L'))
-    kernel = kernel.astype('f')
     for j in range(len(kernel)):
         for i in range(len(kernel[j])):
-            kernel[j][i] = float(kernel[j][i]) - 31946 / (21 * 19)
+            kernel[j][i] = float(kernel[j][i]) - offset
 
-    fixed_image = sg.convolve(gray_image, kernel, mode='same', method='auto')
+    # fixed_image = sg.convolve(gray_image, kernel, mode='same', method='auto')
+    # mask = stam(c_image.copy())
+    # fixed_image[np.logical_not(mask)] += 900
+    # fixed_image[mask] -= 90
+
+    fixed_image = sg.convolve(np.dot(stam(c_image.copy())[..., :3], [0.2125, 0.7154, 0.0721]), kernel, mode='same', method='auto')
+
+    # plt.imshow(fixed_image)
+    # plt.show()
+
 
     ax = plt.subplot(2, 1, 1)
     ax.set_title("Before")
-    # plt.imshow(fixed_image)
-
+    plt.imshow(fixed_image)
 
     max_filter_image = scipy.ndimage.maximum_filter(fixed_image, 50)
     # plt.imshow(max_filter_image)
@@ -122,7 +188,8 @@ def find_tfl_lights(c_image: np.ndarray, cv2=None, **kwargs):
 
     for i in range(len(max_filter_image)):
         for j in range(len(max_filter_image[i])):
-            if max_filter_image[i][j] == fixed_image[i][j] and max_filter_image[i][j] > 1500000: # and check_red(c_image[i][j]):#(np.mean(max_filter_image)*5):
+            if max_filter_image[i][j] == fixed_image[i][j] and max_filter_image[i][
+                j] > 1500000:  # and check_red(c_image[i][j]):#(np.mean(max_filter_image)*5):
                 lst_index1.append(i)
                 lst_index2.append(j)
 
@@ -144,27 +211,39 @@ def show_image_and_gt(image, objs, fig_num=None):
             plt.legend()
 
 
-def test_find_tfl_lights(image_path, json_path=None, fig_num=None):
+def test_find_tfl_lights(image_path, label_path=None, fig_num=None):
     """
     Run the attention code
     """
+
+
     image = np.array(Image.open(image_path))
-    if json_path is None:
-        objects = None
+    objects = None
+    if label_path is None:
+        image_labels = None
     else:
-        gt_data = json.load(open(json_path))
-        what = ['traffic light']
-        objects = [o for o in gt_data['objects'] if o['label'] in what]
+        image_labels = np.array(Image.open(label_path))
 
     show_image_and_gt(image, objects, fig_num)
 
     red_x, red_y, green_x, green_y = find_tfl_lights(image)
+    tfl_images = []
+    no_tfl_images = []
 
-    ax2 = plt.subplot(2, 1, 2)
-    ax2.set_title("finished")
-    # plt.imshow(image)
-    # plt.plot(red_x, red_y, 'rx', markersize=4)
-    # plt.plot(green_x, green_y, 'g+', markersize=4)
+    zeroes = np.zeros((len(image)+80, len(image[0])+80, 3))
+    zeroes[40:image.shape[0]+40, 40:image.shape[1]+40] = image
+    padded_image = zeroes.astype(dtype=np.uint8)
+
+    for x, y in zip(red_x, red_y):
+        if image_labels[y][x] == 19:
+                cropped = padded_image[y:y+80, x:x+80, :]
+                tfl_images.append(cropped)
+        else:
+                cropped = padded_image[y:y+80, x:x+80, :]
+                no_tfl_images.append(cropped)
+    plt.plot(red_x, red_y, 'rx', markersize=4)
+    plt.plot(green_x, green_y, 'g+', markersize=4)
+    return tfl_images, no_tfl_images
 
 
 def main(argv=None):
@@ -202,5 +281,5 @@ def main(argv=None):
 
 if __name__ == '__main__':
     # main()
-    test_find_tfl_lights('cologne_000113_000019_leftImg8bit.png')
+    test_find_tfl_lights('cologne_000113_000019_leftImg8bit.png', 'cologne_000113_000019_gtFine_labelIds.png')
     plt.show(block=True)
--- part2_api.py
+++ part2_api.py
@@ -1,3 +1,5 @@
+import cv2
+
 from main import test_find_tfl_lights
 
 try:
@@ -20,6 +22,11 @@ except ImportError:
     raise
 
 
+
+def create_labels(red_x, red_y, green_x, green_y):
+    pass
+
+
 def main(argv=None):
     """It's nice to have a standalone tester for the algorithm.
     Consider looping over some images from here, so you can manually exmine the results
@@ -31,34 +38,81 @@ def main(argv=None):
     parser.add_argument("-j", "--json", type=str, help="Path to json GT for comparison")
     parser.add_argument('-d', '--dir', type=str, help='Directory to scan images in')
     args = parser.parse_args(argv)
-    default_base = r"C:\Users\ddkil\OneDrive\מסמכים\GitHub\mobileye-part-b-davidteam2\leftImg8bit\test"
-
-    if args.dir is None:
-        args.dir = default_base
+    default_base = r"C:\Users\naorb\Desktop\Scale-up\projects\mobileye\leftImg8bit\leftImg8bit\train"
 
     # for filename in os.listdir(args.dir):
     #     flist = glob.glob(os.path.join(args.dir, filename, '*_leftImg8bit.png'))
     #     for image in flist:
-    #         json_fn = image.replace('_leftImg8bit.png', '_gtFine_polygons.json')
+    #         label_fn = image.replace('_leftImg8bit.png', '_gtFine_polygons.json')
     #
-    #         if not os.path.exists(json_fn):
-    #             json_fn = None
-    #         test_find_tfl_lights(image, json_fn)
+    #         if not os.path.exists(label_fn):
+    #             label_fn = None
+    #         test_find_tfl_lights(image, label_fn)
     counter = 1
-    flist = glob.glob(os.path.join(args.dir, "berlin", '*_leftImg8bit.png'))
+
+    tfl_list = []
+    no_tfl_list = []
+    flist = glob.glob(os.path.join(default_base, "aachen", '*_leftImg8bit.png'))
     for image in flist:
-        json_fn = image.replace('_leftImg8bit.png', '_gtFine_polygons.json')
+        label_fn = image.replace('_leftImg8bit.png', '_gtFine_labelIds.png').replace('leftImg8bit', 'gtFine', 2)
         print(counter)
-        if not os.path.exists(json_fn):
-            json_fn = None
-        test_find_tfl_lights(image, json_fn)
+        if not os.path.exists(label_fn):
+            label_fn = None
+
+        if counter == 8:
+            break
+
+        temp_tfl_list, temp_no_tfl_list = test_find_tfl_lights(image, label_fn)
+        tfl_list += temp_tfl_list
+        no_tfl_list += temp_no_tfl_list
         counter += 1
+
+    tfl_list, no_tfl_list = balanced_quantity(tfl_list, no_tfl_list)
+    labels = [1]*len(tfl_list) + [0]*len(no_tfl_list)
+    data = tfl_list + no_tfl_list
+    data = np.array(data, dtype=np.uint8)
+    data.tofile('data.bin')
+    labels = np.array(labels, dtype=np.uint8)
+    labels.tofile('labels.bin')
 
     if len(flist):
         print("You should now see some images, with the ground truth marked on them. Close all to quit.")
     else:
         print("Bad configuration?? Didn't find any picture to show")
-    plt.show(block=True)
+
+
+def balanced_quantity(tfl_images, no_tfl_images):
+    count = 0
+    mirrored_tfl = []
+    mirrored_no_tfl = []
+    total_len = (len(tfl_images) + len(no_tfl_images)) * 0.4
+
+    while len(no_tfl_images) + len(mirrored_no_tfl) < total_len and count < len(no_tfl_images):
+        mirrored_no_tfl.append(np.fliplr(no_tfl_images[count]))
+        count += 1
+
+    while len(tfl_images) + len(mirrored_tfl) < total_len and count < len(tfl_images):
+        mirrored_tfl.append(np.fliplr(tfl_images[count]))
+        count += 1
+
+    tfl_images += mirrored_tfl
+    no_tfl_images += mirrored_no_tfl
+
+    while len(tfl_images) < (len(tfl_images) + len(no_tfl_images)) * 0.4:
+        no_tfl_images.pop()
+
+    while len(no_tfl_images) < (len(tfl_images) + len(no_tfl_images)) * 0.4:
+        tfl_images.pop()
+
+    return tfl_images, no_tfl_images
+
+
+# loaded_data = np.fromfile("data.bin",  dtype=np.uint8)
+# loaded_labels = np.fromfile("labels.bin",  dtype=np.uint8)
+# a = loaded_data.reshape(len(loaded_data)//19200, 80, 80, 3)
+# for img in a:
+#     plt.imshow(img)
+#     plt.show()
 
 
 if __name__ == '__main__':
